4d7651e327d86e90e91725cf69c5bc4775bc5319
Tsuyoshi Yamaguchi <ronoya442@gmail.com>
2021-03-02 00:14:36 +0900

    Add font

--- src/App.vue
+++ src/App.vue
@@ -16,7 +16,7 @@ export default defineComponent({
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Karla', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
